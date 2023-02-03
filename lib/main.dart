02125e1b9c6d3685d97dba725dc1f31f68c11d4f
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-02-03 19:03:59 +0700

    properly show leaderboard & problems

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'codegames',
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.deepOrange,
         brightness: Brightness.dark,
       ),
       home: const MyHomePage(),
@@ -33,6 +33,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const _normalPadding = 8.0;
+
   final TextEditingController _serverUrl = TextEditingController();
   Future<List<UserPreview>>? _leaderboard;
   Future<List<ProblemPreview>>? _problems;
@@ -44,30 +46,36 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void fetchLeaderboardAndProblems() {
-    _leaderboard = () async {
-      final res =
+    Future<List<UserPreview>> fetchedLeaderboard = () async {
+      http.Response res =
+          await http.get(Uri.parse('${_serverUrl.text}/user/v1'));
+
+      if (res.statusCode != HttpStatus.ok) {
+        throw Exception('Network response not OK! ${res.statusCode}');
+      }
+
+      return (jsonDecode(res.body) as List)
+          .map((element) => UserPreview.fromJson(element))
+          .toList();
+    }();
+
+    Future<List<ProblemPreview>> fetchedProblems = () async {
+      http.Response res =
           await http.get(Uri.parse('${_serverUrl.text}/problems/v1/problem'));
 
       if (res.statusCode != HttpStatus.ok) {
         throw Exception('Network response not OK! ${res.statusCode}');
       }
 
-      return (jsonDecode(res.body) as List<Map<String, dynamic>>)
-          .map((element) => UserPreview.fromJson(element))
-          .toList();
-    }();
-
-    _problems = () async {
-      final res = await http.get(Uri.parse('${_serverUrl.text}/user/v1'));
-
-      if (res.statusCode != HttpStatus.ok) {
-        throw Exception('Network response not OK! ${res.statusCode}');
-      }
-
-      return (jsonDecode(res.body) as List<Map<String, dynamic>>)
+      return (jsonDecode(res.body) as List)
           .map((element) => ProblemPreview.fromJson(element))
           .toList();
     }();
+
+    setState(() {
+      _leaderboard = fetchedLeaderboard;
+      _problems = fetchedProblems;
+    });
   }
 
   @override
@@ -75,62 +83,115 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('codegames'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: _normalPadding,
+            ),
+            child:
+                ElevatedButton(onPressed: () {}, child: const Text('Sign Up')),
+          )
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _serverUrl,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      isDense: true,
-                      labelText: 'Server URL',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _serverUrl,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        isDense: true,
+                        labelText: 'Server URL',
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(width: 8.0),
-                SizedBox(
-                  height: 43.0,
-                  child: ElevatedButton(
-                    onPressed: fetchLeaderboardAndProblems,
-                    child: const Text('Load'),
+                  const SizedBox(width: 8.0),
+                  SizedBox(
+                    height: 43.0,
+                    child: ElevatedButton(
+                      onPressed: fetchLeaderboardAndProblems,
+                      child: const Text('Load'),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            _leaderboard == null
-                ? const Text('Leaderboard not loaded')
-                : FutureBuilder(
-                    future: _leaderboard,
-                    builder: (_, snapshot) {
-                      if (snapshot.hasData) {
-                        return Text(snapshot.data.toString());
-                      }
-                      if (snapshot.hasError) {
-                        return Text(snapshot.error.toString());
-                      }
+                ],
+              ),
+              _leaderboard == null
+                  ? const Text('Leaderboard not loaded')
+                  : FutureBuilder(
+                      future: _leaderboard,
+                      builder: (_, snapshot) {
+                        if (snapshot.hasData) {
+                          return Table(
+                            children: [
+                              const TableRow(
+                                children: [
+                                  Text('Name'),
+                                  Text('Accepted Submission Count'),
+                                ],
+                              ),
+                              ...snapshot.data!.map((userPreview) => TableRow(
+                                    children: [
+                                      Text(userPreview.name),
+                                      Text(userPreview.acceptedSubmissionCount
+                                          .toString()),
+                                    ],
+                                  )),
+                            ],
+                          );
+                        }
+                        if (snapshot.hasError) {
+                          return Text(snapshot.error.toString());
+                        }
 
-                      return const CircularProgressIndicator();
-                    }),
-            _problems == null
-                ? const Text('Problems not loaded')
-                : FutureBuilder(
-                    future: _problems,
-                    builder: (_, snapshot) {
-                      if (snapshot.hasData) {
-                        return Text(snapshot.data.toString());
-                      }
-                      if (snapshot.hasError) {
-                        return Text(snapshot.error.toString());
-                      }
+                        return const CircularProgressIndicator();
+                      }),
+              _problems == null
+                  ? const Text('Problems not loaded')
+                  : FutureBuilder(
+                      future: _problems,
+                      builder: (_, snapshot) {
+                        if (snapshot.hasData) {
+                          return Column(
+                            children: snapshot.data!
+                                .map((problemPreview) => Card(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(
+                                          _normalPadding,
+                                        ),
+                                        child: Column(
+                                          children: [
+                                            Text(
+                                              problemPreview.title,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .titleLarge,
+                                            ),
+                                            Text(problemPreview.description),
+                                            OutlinedButton(
+                                              onPressed: () {},
+                                              child: const Text('Go'),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ))
+                                .toList(),
+                          );
+                        }
+                        if (snapshot.hasError) {
+                          return Text(snapshot.error.toString());
+                        }
 
-                      return const CircularProgressIndicator();
-                    }),
-          ],
+                        return const CircularProgressIndicator();
+                      }),
+            ],
+          ),
         ),
       ),
     );
